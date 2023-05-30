cf2abeb473fef2b3e836d60ffa4b966ab812f91c
ammurana4216 <drona.ambeshwar@gmail.com>
2023-05-30 10:39:19 +0530

    ReactState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Header from "./components/Header";
 import ReactState from "./components/ReactState";
+
+// import { func1 } from './components/abc';
+
 function App(){
-  //let title = "React Course";
-  //let bgClass = "bg-danger";
+  let title = "React Course";
+  let bgClass = "bg-danger";
+
   let isHeading = true;
   let size = 24;
   return(
     <>
     <div className={isHeading ? "bg-danger":""}>
-      Hello React World
-      <p style={{color:"yellow", fontSize:size+""}}>{1+1}</p>
-      <Header />
-      <ReactState/>
-      helo
+   <ReactState />
     </div>
     </>
   )
