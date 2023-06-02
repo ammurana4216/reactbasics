e865f2a9e392187bb33c0f6968434f49f8a94b1b
ammurana4216 <drona.ambeshwar@gmail.com>
2023-06-02 10:24:22 +0530

    src/components/StudentRow.jsx

--- src/components/StudentRow.jsx
+++ src/components/StudentRow.jsx
@@ -1,12 +1,12 @@
 function StudentRow(props){
-    const {item} = props ; 
+    const {item , deleteStudent} = props ; 
     return(
         <tr key={item.id}>
         <td>{item.id}</td>
         <td>{item.name}</td>
         <td>{item.fees}</td>
         <td>{item.course}</td>
-       {/*<td><button onClick={(deleteStudent)}></button></td>*/}
+       <td><button onClick={()=>deleteStudent(item.id)}> Delete</button></td>
     </tr>
     )
 }
